25f6fc8d39a85e1afe44fc8dab1b9eeba4c16e92
KRQuinn <kr.quinn82@gmail.com>
2021-10-28 15:58:26 -0700

    test

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
+
 app.listen(PORT, () => {
         console.log(`listening on ${PORT}`)
 })

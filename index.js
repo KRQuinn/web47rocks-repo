a121d32c1e1d8fb160bbc1e2f4fd0a9cd375302b
KRQuinn <kr.quinn82@gmail.com>
2021-10-28 15:54:36 -0700

    add react

--- index.js
+++ index.js
@@ -1,46 +1,22 @@
 require('dotenv').config()
-
+const path = require('path')
 const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client/build')))
+
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-    res.send(`
-        <h1>Web ${process.env.COHORT} rocks!</h1>
-    `)
+app.get('api/foo', (req, res) => {
+    res.json({message: 'hello there!'})
 })
 
-app.get('/port', (req, res) => {
-    res.send(`
-        <div>port is ${PORT}</div>
-    `)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
-
-
 
 app.listen(PORT, () => {
         console.log(`listening on ${PORT}`)
 })
-
-
-
-
-
-function cohortRocks() { // eslint-disable-line
-    console.log('arguments:', process.argv.slice(2))
-    console.log(`web ${process.argv[2]} rocks`)
-}
-function cohortRocksEnv(){
-    console.log(`home folder is ${process.env.HOME}`)
-    console.log(`my shell is ${process.env.SHELL}`)
-    console.log(`my OS is ${process.env.OS}`)
-    console.log(`my host name is ${process.env.HOSTNAME}`)
-    console.log(`my username is ${process.env.USERNAME}`)
-
-    console.log(`In my environment FOO is "${process.env.FOO}"`)
-    console.log(`My super secret comes from "${process.env.SUPER_SECRET}""`)
-
-}
-cohortRocksEnv()

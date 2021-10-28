c4735fea45f0de7406149840401c50dee59aa3c0
KRQuinn <kr.quinn82@gmail.com>
2021-10-28 15:16:55 -0700

    wip

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send(`
-        <h1>Web 47 rocks!</h1>
+        <h1>Web ${process.env.COHORT} rocks!</h1>
     `)
 })
 
@@ -17,6 +17,8 @@ app.get('/port', (req, res) => {
         <div>port is ${PORT}</div>
     `)
 })
+
+
 
 app.listen(PORT, () => {
         console.log(`listening on ${PORT}`)
